06496b492ac87a712f914e39f2f91115c9d51a52
Daria <sadaskeanu@gmail.com>
2022-09-01 21:30:38 +0200

    install Apollo, create component App and style it

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import './App.css'
+import { useQuery, gql } from '@apollo/client'
+
+export const AnimeList = gql`
+  {
+    Page {
+      media {
+        siteUrl
+        title {
+          english
+          native
+        }
+        description
+        coverImage {
+          medium
+        }
+        bannerImage
+        volumes
+        episodes
+      }
+    }
+  }
+`
+
+function App() {
+  const { loading, error, data } = useQuery(AnimeList)
+
+  console.log(data?.Page?.media[0])
+  if (loading) return <> Loading </>
+  if (error) return <>{JSON.stringify(error)}</>
+  return (
+    <div className="container">
+      <h1> 🐈 Anime List </h1>
+      <hr width="80%" />
+      {data?.Page?.media.map(anime => (
+        <>
+          <div className="card">
+            <img src={anime.coverImage.medium} alt="anime" />
+            <div>
+              <h1>{anime.title.english} </h1>
+              <div className="episodes">
+                Episodes <b>{anime.episodes} </b>
+              </div>
+              <div
+                dangerouslySetInnerHTML={{ __html: anime.description }}
+              ></div>
+            </div>
+          </div>
+          <hr width="75%" />
+        </>
+      ))}
+    </div>
+  )
+}
+
+export default App
